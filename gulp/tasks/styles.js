e942b60f01fcc7ab18b62eccae471b6a926a9f31
Martin <tjenamartin@gmail.com>
2018-04-03 17:46:45 +0200

    header and mobile header complete

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,16 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 
 gulp.task ('styles' ,function(){
    return gulp.src('./app/assets/styles/styles.css')
-       .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+       .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
        .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
